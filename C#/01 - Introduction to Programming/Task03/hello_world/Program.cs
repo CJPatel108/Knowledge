485cb7734d5e3375191df40bb2ba91a39a863426
CJPatel <cjai.patel@gmail.com>
2022-09-05 12:45:25 +0200

    Adding C# First Program

---
+++ C#/01 - Introduction to Programming/Task03/hello_world/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Please enter your name: ");
+string name = Console.ReadLine();
+Console.WriteLine(name);
+Console.Write("Please enter your age: ");
+string age = Console.ReadLine();
+Console.WriteLine(age);
+Console.WriteLine("Hello World!");
---
+++ C#/01 - Introduction to Programming/Task03/menu/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter your 3 favourite food items");
+
+Console.Write("Item 1: ");
+string item1 = Console.ReadLine();
+Console.Write("Item 2: ");
+string item2 = Console.ReadLine();
+Console.Write("Item 3: ");
+string item3 = Console.ReadLine();
+
+Console.WriteLine();
+Console.WriteLine("Order Confirmation! You have ordered:");
+Console.WriteLine(item1);
+Console.WriteLine(item2);
+Console.WriteLine(item3);
