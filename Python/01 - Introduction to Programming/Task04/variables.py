a48d0cfa3072938c92b1b971bc77d33dccb7804f
CJPatel <cjai.patel@gmail.com>
2022-09-06 11:29:07 +0200

    Variables - Storing Data in Programs

---
+++ Python/01 - Introduction to Programming/Task04/variables.py
@@ -0,0 +1,14 @@
+#Task4 - variables.py
+#------------------------------------------------------------#
+string_name = "John" #initialising a string variable
+char_symbol = '(' #initialising a character variable
+int_num = 24 + 3 #initialising an integer variable
+float_decimal = 0.73 + 0.04 #initialising a float variable
+boolean_true = False #initialising a boolean variable
+
+#printing all variables
+print(string_name)
+print(char_symbol)
+print(int_num)
+print(float_decimal)
+print(boolean_true)
