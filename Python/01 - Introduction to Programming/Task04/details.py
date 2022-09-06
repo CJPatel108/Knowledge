20aa7cee750a24517b4b75267f73fb78f3fcf6dd
CJPatel <cjai.patel@gmail.com>
2022-09-06 20:32:19 +0200

    Variables - Storing Data in Programs

---
+++ Python/01 - Introduction to Programming/Task04/details.py
@@ -0,0 +1,9 @@
+#Task4 - details.py
+#------------------------------------------------------------#
+name = input("Please enter your Name: ") #initialising a string variable
+age = input("Please enter your Age: ") #initialising a variable
+house_number = input("Please enter your House Number: ") #initialising a variable
+street_name = input("Please enter your Street Name: ") #initialising a variable
+
+#printing all variables
+print("This is "+ name + " he is " + age + " years old and lives at house number " + house_number + " on " + street_name)
