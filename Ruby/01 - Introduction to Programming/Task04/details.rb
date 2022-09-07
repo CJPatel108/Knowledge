739b6e334c8057e0dedf5508c0843b7b767c21dc
CJPatel <cjai.patel@gmail.com>
2022-09-07 14:37:56 +0200

    Variables - Storing Data in Programs (Ruby)

---
+++ Ruby/01 - Introduction to Programming/Task04/details.rb
@@ -0,0 +1,13 @@
+#Task4 - details.rb
+#------------------------------------------------------------#
+print "Please enter your Name: "
+name = gets.chomp() #initialising a string variable
+print "Please enter your Age: "
+age = gets.chomp() #initialising a variable
+print "Please enter your House Number: "
+house_number = gets.chomp() #initialising a variable
+print "Please enter your Street Name: "
+street_name = gets.chomp() #initialising a variable
+
+#printing all variables
+print "This is "+ name + " he is " + age + " years old and lives at house number " + house_number + " on " + street_name
---
+++ Ruby/01 - Introduction to Programming/Task04/variables.rb
@@ -0,0 +1,13 @@
+#Task4 - variables.rb
+#------------------------------------------------------------#
+string_name = "John" 
+char_symbol = '(' 
+int_num = 24 + 3 #initialising an integer variable
+float_decimal = 0.73 + 0.04 #initialising a float variable
+boolean_true = false #initialising a boolean variable
+
+puts string_name
+puts char_symbol
+puts int_num
+puts float_decimal
+puts boolean_true
