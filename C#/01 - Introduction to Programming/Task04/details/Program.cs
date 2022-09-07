01da5857f4017d2bd8396a206437f0903606ea2c
CJPatel <cjai.patel@gmail.com>
2022-09-07 15:28:31 +0200

    C# Update

---
+++ C#/01 - Introduction to Programming/Task04/details/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Please enter your Name: ");
+string name = Console.ReadLine();
+Console.Write("Please enter your Age: ");
+string age = Console.ReadLine();
+Console.Write("Please enter your House Number: ");
+string house_number = Console.ReadLine();
+Console.Write("Please enter your Street Name: ");
+string street_name = Console.ReadLine();
+
+Console.WriteLine("This is "+ name + " he is " + age + " years old and lives at house number " + house_number + " on " + street_name);
---
+++ C#/01 - Introduction to Programming/Task04/variables/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "John";
+char symbol = '(';
+int num = 24 + 3;
+double numDecimal = 0.73 + 0.04;
+bool isTrue = false;
+
+Console.WriteLine(name);
+Console.WriteLine(symbol);
+Console.WriteLine(num);
+Console.WriteLine(numDecimal);
+Console.WriteLine(isTrue);
