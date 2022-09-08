cf32706af7cf82cf8875037c8c6f5310e3f25aaa
CJPatel <cjai.patel@gmail.com>
2022-09-08 18:41:20 +0200

    The String Data Type

---
+++ Java/01 - Introduction to Programming/Task05/strings.java
@@ -0,0 +1,17 @@
+//Task5 - strings.java
+/************************************************************/
+//import java.util.*;
+
+public class strings
+{
+    public static void main(String[] args)
+    {
+        //declare string variable
+        String hero = "Super Man";
+        //convert all characters of string to upper case
+        hero = hero.toUpperCase();
+        //print output in specified manner
+        System.out.println(hero.charAt(0) + "^" + hero.charAt(1) + "^" + hero.charAt(2) + "^" + hero.charAt(3) + "^" + hero.charAt(4) + hero.charAt(5) + hero.charAt(6) + "^" + hero.charAt(7) + "^" + hero.charAt(8));
+
+    }
+}
