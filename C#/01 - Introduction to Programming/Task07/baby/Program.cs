cc2c86fe2dd378a12aa0bc00a9fda54427b9afb6
CJPatel <cjai.patel@gmail.com>
2022-09-12 21:40:05 +0200

    Control Structures - if Statements (C#)

---
+++ C#/01 - Introduction to Programming/Task07/baby/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//request input from user
+Console.WriteLine("Please enter the year that you were born in: ");
+int DOY = Int32.Parse(Console.ReadLine());
+
+//calculation of age and display appropriate message
+if (2020-DOY >= 18)
+{
+    Console.WriteLine("Congrats you are old enough");
+}
+// else
+// {
+//     Console.WriteLine("You are too young");
+// }
