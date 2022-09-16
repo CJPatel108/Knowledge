c4490e1c943df47b90a0f59dd6d3a07551915322
CJPatel <cjai.patel@gmail.com>
2022-09-16 11:22:20 +0200

    Control Structures (C# Update)

---
+++ C#/01 - Introduction to Programming/Task10/control/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//request input from user
+Console.Write("What is your age? ");
+int age = Convert.ToInt32(Console.ReadLine());
+
+//print message according to age
+if (age>=18)
+{
+    Console.Write("You are old enough!");
+}
+else if (age>16)
+{
+    Console.Write("Almost there");
+}
+else 
+{
+    Console.Write("You're just too young!");
+}
