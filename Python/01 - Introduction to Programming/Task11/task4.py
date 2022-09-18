bf84da66a27eabdba9e1736bd25942513765fe4c
CJPatel <cjai.patel@gmail.com>
2022-09-18 18:40:04 +0200

    Update

--- Python/01 - Introduction to Programming/Task11/task4.py
+++ Python/01 - Introduction to Programming/Task11/task4.py
@@ -3,7 +3,6 @@
 #request input from user
 num = int(input("Please enter an integer: "))
 
-
 #determine if num is divisible by 2 and 5, by 2 or 5, or by neither
 if(num%2==0 and num%5==0):
 	print(str(num) + " is divisible by 2 and 5")
