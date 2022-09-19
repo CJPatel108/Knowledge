f9e3004bde165836a1276da794bff6096f76d183
CJPatel <cjai.patel@gmail.com>
2022-09-19 11:03:02 +0200

    Logical Operations (C#)

---
+++ C#/01 - Introduction to Programming/Task11/Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num;
+//request input from user
+Console.Write("Please enter an integer: ");
+num = Int32.Parse(Console.ReadLine());
+
+//determine if num is divisible by 2 and 5, by 2 or 5, or by neither
+if (num%2==0 && num%5==0)
+{
+    Console.WriteLine(num + " is divisible by 2 and 5");
+}
+else if (num%2==0 || num%5==0)
+{
+    Console.WriteLine(num + " is divisible by 2 or 5");
+}
+else
+{
+    Console.WriteLine(num + " is not divisible by 2 or 5");
+}
