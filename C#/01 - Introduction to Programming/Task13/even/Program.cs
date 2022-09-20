406a3aecd964f37ab060e29a61f0a5fa1562f18d
CJPatel <cjai.patel@gmail.com>
2022-09-20 11:36:55 +0200

    Control Structures (C#)

---
+++ C#/01 - Introduction to Programming/Task13/even/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//request input from user
+Console.Write("Please enter a number: ");
+int num = Int32.Parse(Console.ReadLine());
+
+//initialising first even number as 2
+int even = 2;
+
+//prints out all even numbers from 1 up to and possibly including num
+while (even<=num)
+{
+    Console.WriteLine(even);
+    even+=2;
+}
