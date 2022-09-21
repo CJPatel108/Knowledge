3ed3b1265ee2885578a55841e027146e038dc148
CJPatel <cjai.patel@gmail.com>
2022-09-21 19:31:40 +0200

    Logical Operations (Ruby - Update)

--- Ruby/01 - Introduction to Programming/Task11/Task4.rb
+++ Ruby/01 - Introduction to Programming/Task11/Task4.rb
@@ -5,9 +5,9 @@ print "Please enter an integer: "
 num = gets.chomp().to_i
 
 #determine if num is divisible by 2 and 5, by 2 or 5, or by neither
-if num%2==0 and num%5==0
+if num%2==0 && num%5==0
 	puts num.to_s + " is divisible by 2 and 5"
-elsif num%2==0 or num%5==0
+elsif num%2==0 || num%5==0
 	puts num.to_s + " is divisible by 2 or 5"
 else
 	puts num.to_s + " is not divisible by 2 or 5"
