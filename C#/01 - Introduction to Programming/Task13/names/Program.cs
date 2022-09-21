18c95f1b271aa2ffd4019d522d0797a088508e6b
CJPatel <cjai.patel@gmail.com>
2022-09-21 19:10:20 +0200

    Control Structures (C#)

---
+++ C#/01 - Introduction to Programming/Task13/names/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//request input from user
+int i = 0;
+string name = "";
+while (!name.Equals("stop"))
+{
+    Console.Write("Please enter the names of all pupils in class: ");
+    name = Console.ReadLine().ToLower();
+    i+=1;
+}
+
+Console.WriteLine("Number of pupils in class: " + (i-1));
