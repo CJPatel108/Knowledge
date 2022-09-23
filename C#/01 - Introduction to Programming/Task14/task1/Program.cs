cd992d56a33e7469f9944a7b284081b9b4ca2241
CJPatel <cjai.patel@gmail.com>
2022-09-23 12:01:49 +0200

    Control Structures (C#)

---
+++ C#/01 - Introduction to Programming/Task14/task1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//request input from user
+Console.Write("Please enter a number: ");
+int num = Int32.Parse(Console.ReadLine());
+int[] range = {1,2,3,4,5,6,7,8,9,10,11,12};
+
+foreach (int i in range)
+{
+    Console.WriteLine(num + " x " + i + " =  " + num*i);
+}
