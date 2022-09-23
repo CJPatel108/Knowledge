f4a16af07c6b46ab5636e59e5b6c0301ff1a2ba9
CJPatel <cjai.patel@gmail.com>
2022-09-23 12:00:43 +0200

    C++

---
+++ C++/01 - Introduction to Programming/Task03/hello_world.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    string age;
+
+    cout<<"Please enter your name: ";
+    cin>>name;
+    cout<<name;
+    cout<<endl;
+    cout<<"Please enter your age: ";
+    cin>>age;
+    cout<<age;
+    cout<<endl;
+    cout<<"Hello World!"<<endl;
+
+    return 0;
+}
