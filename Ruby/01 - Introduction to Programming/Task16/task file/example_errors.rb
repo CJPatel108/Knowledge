3ecf800512b8a6dd15d045eb096f493a41814af0
CJPatel <cjai.patel@gmail.com>
2022-09-27 15:59:46 +0200

    Defensive Programming (Ruby)

---
+++ Ruby/01 - Introduction to Programming/Task16/task file/example_errors.rb
@@ -0,0 +1,12 @@
+# This example program is meant to demonstrate errors.
+ 
+# There are some errors in this program, try run the program by pressing F5.
+# Now look at the error messages and find and fix the errors.
+ 
+name = "Tim"
+surname = " Jones" #syntax error - incorrect space indentation; #possible logical error - extra spacing in beginning of string
+age = 21
+ 
+fullMessage = name + surname + " is " + age.to_s + " years old" #Runtime error - variable age needs to be converted to string; #syntax error - 'is' is under the wrong syntax, needs to be within quotations; #possible logical error - missing appropriate spacing between name and surname;
+ 
+puts fullMessage #syntax error - missing parenthesis for python3;
