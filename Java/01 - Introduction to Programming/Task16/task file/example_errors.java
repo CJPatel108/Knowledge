099b2d4ab5716d249fc5d86b057eb1852ade889a
CJPatel <cjai.patel@gmail.com>
2022-09-27 15:59:28 +0200

    Defensive Programming (Java)

---
+++ Java/01 - Introduction to Programming/Task16/task file/example_errors.java
@@ -0,0 +1,22 @@
+// import java.util.*;
+
+public class example_errors
+{
+    public static void main(String[] args)
+    {
+        // This example program is meant to demonstrate errors.
+
+        // There are some errors in this program, try run the program by pressing F5.
+        // Now look at the error messages and find and fix the errors.
+
+        //initialise variables
+        String name = "Tim";
+        String surname = " Jones"; //syntax error - incorrect space indentation; //possible logical error - extra spacing in beginning of string
+        int age = 21;
+
+        String fullMessage = name + surname + " is " + age + " years old"; //Runtime error - variable age needs to be converted to string; //syntax error - 'is' is under the wrong syntax, needs to be within quotations; //possible logical error - missing appropriate spacing between name and surname;
+
+        System.out.println(fullMessage); //syntax error - missing parenthesis for python3;
+
+    }
+}
