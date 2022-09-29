1991ff22dedec579df868e49691c272b50d15907
CJPatel <cjai.patel@gmail.com>
2022-09-29 16:03:13 +0200

    Defensive Programming (C#)

---
+++ C#/01 - Introduction to Programming/Task16/task file/example_errors/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// This example program is meant to demonstrate errors.
+
+// There are some errors in this program, try run the program by pressing F5.
+// Now look at the error messages and find and fix the errors.
+
+//initialise variables
+string name = "Tim";
+string surname = " Jones"; //syntax error - incorrect space indentation; //possible logical error - extra spacing in beginning of string
+int age = 21;
+
+string fullMessage = name + surname + " is " + age + " years old"; //Runtime error - variable age needs to be converted to string; //syntax error - 'is' is under the wrong syntax, needs to be within quotations; //possible logical error - missing appropriate spacing between name and surname;
+
+Console.WriteLine(fullMessage); //syntax error - missing parenthesis;
