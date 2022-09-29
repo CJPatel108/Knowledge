c67a4cefaf8875c91f5e49df3940158d905edf51
CJPatel <cjai.patel@gmail.com>
2022-09-29 16:04:17 +0200

    String Handling (Ruby)

---
+++ Ruby/01 - Introduction to Programming/Task17/alternative.rb
@@ -0,0 +1,18 @@
+#Task17 - alternative.rb
+#------------------------------------------------------------#
+#request input from user
+print "Please enter a value for a string variable: "
+original = gets.chomp()
+alt = ""
+
+# make each alternate character an uppercase character and each other alternate character a lowercase character.
+for i in Range.new(0, original.length)
+	if i%2==0
+		alt += original[i].upcase() #******************************************************
+	else
+		alt += original[i].downcase() #******************************************************
+    end
+end
+
+#display result
+puts alt
