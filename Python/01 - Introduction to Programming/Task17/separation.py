d2b7fec4eb9bf9bc59ff082b514dc0e9e5634a44
CJPatel <cjai.patel@gmail.com>
2022-10-01 11:24:27 +0200

    Update

--- Python/01 - Introduction to Programming/Task17/separation.py
+++ Python/01 - Introduction to Programming/Task17/separation.py
@@ -1,10 +1,10 @@
-#Task17 - seperation.py
+#Task17 - separation.py
 #------------------------------------------------------------#
 #request input from user
 original = input("Please enter a value for a string variable: ")
 
 #replace all space characters with newline
-seperation = original.replace(" ", "\n")
+separation = original.replace(" ", "\n")
 
 #display result
-print(seperation)
+print(separation)
