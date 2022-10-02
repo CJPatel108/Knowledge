e365dad7ba63da27cc3137560eb3319e30643109
CJPatel <cjai.patel@gmail.com>
2022-10-02 17:30:20 +0200

    String Handling (Ruby)

---
+++ Ruby/01 - Introduction to Programming/Task17/disappear.rb
@@ -0,0 +1,16 @@
+#Task17 - disappear.rb
+#------------------------------------------------------------#
+#request input from user
+print "Please enter a value for a string variable: "
+original = gets.chomp()
+print "Which characters would you like to make disappear? "
+characters = gets.chomp() #to be entered without commas or spaces
+#characters = characters.gsub(",","") ; if characters are seperated by commas
+disappear = original
+
+#remove specified characters
+for i in Range.new(0,characters.length)
+  disappear = disappear.gsub(characters[i],"")
+end
+
+puts disappear
