4f8f66b2074f8d2e5f6977b035382051d99dcc2c
CJPatel <cjai.patel@gmail.com>
2022-10-03 14:09:48 +0200

    String Handling (C#)

---
+++ C#/01 - Introduction to Programming/Task17/separation/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//request input from user
+Console.WriteLine("Please enter a value for a string variable: ");
+string original = Console.ReadLine();
+
+//replace all space characters with newline
+string separation = original.Replace(" ", "\n");
+
+//display result
+Console.WriteLine(separation);
