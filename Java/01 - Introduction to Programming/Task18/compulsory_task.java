8f7ee5f1d7f103502df98c83f8dced514baf4d47
CJPatel <cjai.patel@gmail.com>
2022-10-09 15:58:10 +0200

    External Data Sources - Input (Java)

--- Java/01 - Introduction to Programming/Task18/compulsory_task.java
+++ Java/01 - Introduction to Programming/Task18/compulsory_task.java
@@ -1,9 +1,27 @@
 import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class compulsory_task
 {
     public static void main(String[] args)
     {
+        String data;
+        try
+        {
+            File filename = new File("DOB.txt");
+            Scanner myReader = new Scanner(filename);
+            while(myReader.hasNextLine())
+            {
+                data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        }catch (FileNotFoundException e)
+        {
+                System.out.println("An error has occurred");
+                e.printStackTrace();
+        }
 
     }
 }
