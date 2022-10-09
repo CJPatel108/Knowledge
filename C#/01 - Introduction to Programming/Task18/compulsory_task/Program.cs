6356b175ec9f6c1e6b42f1d3eef08c980b51c146
CJPatel <cjai.patel@gmail.com>
2022-10-09 16:06:20 +0200

    External Sources - Input (C#)

--- C#/01 - Introduction to Programming/Task18/compulsory_task/Program.cs
+++ C#/01 - Introduction to Programming/Task18/compulsory_task/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+string text = System.IO.File.ReadAllText("DOB.txt");
+
+Console.WriteLine(text);
+
+string [] lines = System.IO.File.ReadAllLines("DOB.txt");
+foreach (string line in lines)
+{
+    Console.WriteLine("\t + line");
+}
