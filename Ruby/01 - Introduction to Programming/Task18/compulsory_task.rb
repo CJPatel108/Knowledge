7a80ad2d13f37c9308f4698c39c66cdf4552c97d
CJPatel <cjai.patel@gmail.com>
2022-10-09 16:06:04 +0200

    External Sources - Input (Ruby)

---
+++ Ruby/01 - Introduction to Programming/Task18/compulsory_task.rb
@@ -0,0 +1,55 @@
+#Task18 - compulsory_task.rb
+#------------------------------------------------------------#
+filename = "DOB.txt"
+content = File.read(filename);
+puts content
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #instantiate variables for names, surnames and DOBs
+# contents = ""
+# name = ""
+# name = name.split()
+# surname = ""
+# surname = surname.split()
+# DOB = ""
+# DOB = DOB.split()
+# i = 0
+
+# #open file and read data into appropriate lists
+# f = open('DOB.txt', 'r+')
+# for line in f:
+#   #contents = contents + line
+#   contents = line.split()
+#   name.append(contents[i])
+#   surname.append(contents[i+1])
+#   DOB.append(str(contents[i+2]) + " " + contents[i+3] + " " + contents[i+4])
+#   i+=i
+# #close file
+# f.close()
+
+# #display Names
+# print("Name")
+# for i in range (0, len(name)):
+#   print(name[i][0] + " " + surname[i])
+# print()
+
+# #display Birthdates
+# print("Birthdate")
+# for i in range (0, len(name)):
+#   print(DOB[i])
+# print()
