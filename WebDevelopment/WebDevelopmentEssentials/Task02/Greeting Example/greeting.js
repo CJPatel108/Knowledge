de773077279d0e03eb4321c20addd858503718d7
CJPatel <cjai.patel@gmail.com>
2023-03-14 22:09:55 +0200

    JavaScript First Program

---
+++ WebDevelopment/WebDevelopmentEssentials/Task02/Greeting Example/greeting.js
@@ -0,0 +1,15 @@
+//CJ Patel
+//Task2 - greeting.js (Compulsory Task 1)
+//------------------------------------------------------------
+//request input from user
+let userName = prompt("Please enter your name?");
+//display name
+console.log("Hi, " + userName);
+//request input from user
+let userAge = prompt("Please enter your age?");
+//display age
+console.log("Age Entered: " + userAge);
+//display newline character
+console.log("\n");
+//display string
+console.log("It's nice to meet you!");
