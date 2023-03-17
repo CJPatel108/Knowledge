f41110a24db787d7829695690b656407a79161fe
CJPatel <cjai.patel@gmail.com>
2023-03-17 13:40:55 +0200

    JavaScript First Program

---
+++ WebDevelopment/WebDevelopmentEssentials/Task02/Shopping Example/shopping.js
@@ -0,0 +1,12 @@
+//CJ Patel
+//Task2 - shopping.js (Compulsory Task 2)
+//------------------------------------------------------------
+//request input from user
+let item1 = prompt("Please enter 3 items for your Shopping List:\nPlease enter item number 1: ")
+let item2 = prompt("Please enter item number 2: ")
+let item3 = prompt("Please enter item number 3: ")
+//display shopping list
+console.log("My Shopping List:")
+console.log(item1)
+console.log(item2)
+console.log(item3)
